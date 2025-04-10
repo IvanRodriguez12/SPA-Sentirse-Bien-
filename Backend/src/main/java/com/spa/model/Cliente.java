74c19dc1d922d94f2c1dfcf2e934e57330c41525
desa <ivanrosriguez7@gmail.com>
2025-04-09 21:55:08 -0300

    template backend

---
+++ Backend/src/main/java/com/spa/model/Cliente.java
@@ -0,0 +1,16 @@
+package com.spa.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    // Getters y Setters
+}
