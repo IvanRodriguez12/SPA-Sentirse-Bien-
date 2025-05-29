738a01e29f193bb331333178cc325dd393260658
desa <ivanrosriguez7@gmail.com>
2025-05-29 04:00:48 -0300

    Optimizacion 3

--- Backend/src/main/java/com/spa/SpaBackendApplication.java
+++ Backend/src/main/java/com/spa/SpaBackendApplication.java
@@ -2,10 +2,25 @@ package com.spa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class SpaBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpaBackendApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(() -> {
+            try {
+                Thread.sleep(10000); // Máximo 10 segundos de espera
+            } catch (InterruptedException ignored) {}
+        });
     }
 }
