a0141a63b6d52866f3e98416cc5e629dd519529b
Ignacio <ibianchiberman@gmail.com>
2025-06-13 14:01:43 -0300

    import

--- client/src/pages/admin/AdminLogin.jsx
+++ client/src/pages/admin/AdminLogin.jsx
@@ -1,5 +1,4 @@
-import "./AdminLogin.css";
-import '../styles/AdminLogin.css';
+import '../../styles/AdminLogin.css';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
