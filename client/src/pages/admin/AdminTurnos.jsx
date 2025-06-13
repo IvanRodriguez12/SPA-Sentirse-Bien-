a37e79f598e1b0c331fe79c31bc19826ff7944c8
Ignacio <ibianchiberman@gmail.com>
2025-06-13 16:51:22 -0300

    CrearTurnos x4

--- client/src/pages/admin/AdminTurnos.jsx
+++ client/src/pages/admin/AdminTurnos.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import Modal from 'react-modal';
 import '../../styles/admin.css';
+import CrearTurno from '../../components/CrearTurno';
 import AdminHeader from './AdminHeader';
 
 Modal.setAppElement('#root');
